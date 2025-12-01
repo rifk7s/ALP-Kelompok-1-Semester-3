fc977cea79cd1859e9a1c4ad1fd0d44cf4ac1181
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-12-01 13:38:45 +0800

    updateUser&ProductFiles&ItsMigrationFiles

--- backend/database/migrations/2025_11_28_131656_create_products_table.php
+++ backend/database/migrations/2025_11_28_131656_create_products_table.php
@@ -15,29 +15,27 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('variety');
-            $table->date('harvest_date')->nullable();
+            $table->date('harvest_date');
             $table->integer('storage_days');
             $table->decimal('price_per_kg');
             $table->decimal('stock_kg');
-            $table->decimal('committed_kg')->default(0); // How many kgs are already pre-ordered
-            $table->decimal('available_kg')->storedAs('stock_kg - committed_kg');; // stock_kg - committed_kg
-            $table->enum('sale_type', ['pre_order', 'ready_stock']);
-            $table->integer('lead_time_days')->nullable(); // Relevant for pre_order type
-            $table->text('description')->nullable();
-            $table->enum('status', ['active', 'inactive', 'sold_out']);
-            $table->boolean('managed_by_bumdes')->default(false);
-            $table->enum('seller_type', ['bumdes', 'petani']);
-            $table->string('seller_display');
+            // $table->decimal('committed_kg')->default(0); // How many kgs are already pre-ordered
+            // $table->decimal('available_kg')->storedAs('stock_kg - committed_kg');; // stock_kg - committed_kg
+            // $table->enum('sale_type', ['pre_order', 'ready_stock']);
+            // $table->integer('lead_time_days')->nullable(); // Relevant for pre_order type
+            $table->text('description');
+            $table->enum('status', ['active', 'sold_out']);
+            // $table->boolean('managed_by_bumdes')->default(false);
+            // $table->enum('seller_type', ['bumdes', 'petani']);
+            // $table->string('seller_display');
             $table->decimal('sold_kg')->default(0);
             $table->timestamps();
         });
 
-        Schema::table('products', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            // $table->foreignId('created_by')->constrained()->onDelete('cascade');
-            // $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            // $table->foreignId('seller_id')->nullable()->constrained()->onDelete('cascade');
-        });
+        // Schema::table('products', function (Blueprint $table) {
+        //     $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        //     $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        // });
     }
 
     /**
@@ -47,11 +45,11 @@ return new class extends Migration
     {
         Schema::dropIfExists('products');
 
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropConstrainedForeignId('user_id');
-            // $table->dropConstrainedForeignId('created_by');
-            // $table->dropConstrainedForeignId('category_id');
-            // $table->dropConstrainedForeignId('seller_id');
-        });
+        // Schema::table('products', function (Blueprint $table) {
+        //     $table->dropConstrainedForeignId('user_id');
+        //     // $table->dropConstrainedForeignId('created_by');
+        //     // $table->dropConstrainedForeignId('category_id');
+        //     // $table->dropConstrainedForeignId('seller_id');
+        // });
     }
 };
