85eba23d815dcdf9268ab41d9072623779e0f8df
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-12-02 22:00:08 +0800

    addRelationshipBetweenCategory&Product

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -25,7 +26,11 @@ class Product extends Model
         'status',
 
         // Foreign keys
-        // 'user_id',
-        // 'category_id',
+        'category_id',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
--- backend/database/migrations/2025_11_28_131738_create_categories_table.php
+++ backend/database/migrations/2025_11_28_131738_create_categories_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->string('icon')->nullable();
             $table->timestamps();
         });
