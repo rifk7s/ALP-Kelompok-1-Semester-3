22cbd4c909b7257403a307a9aacf0e122be2e7f1
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-12-03 08:45:29 +0800

    addControllersForCategory&ProductImages&TheApiRoutes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,13 +2,18 @@
 
 // use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductImageController;
 use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-// Public routes (no authentication required)
+/*
+    Authentication Routes
+*/
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
@@ -17,3 +22,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('/me',      [AuthController::class, 'me']);
 });
+
+
+/*
+    Product Routes
+*/
+Route::apiResource('/products/product', ProductController::class);
+Route::apiResource('/products/categories', CategoryController::class);
+Route::apiResource('/products/product-images', ProductImageController::class);
