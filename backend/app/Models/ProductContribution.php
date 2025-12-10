8a5c7d2615e3950654baa0993db6e2594ef31f4d
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-12-10 12:04:26 +0800

    productContributionModel

--- backend/app/Models/ProductContribution.php
+++ backend/app/Models/ProductContribution.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductContribution extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+
+    protected $fillable = [
+        'contributed_kg',
+        'remaining_kg',
+        'entry_date',
+        'harvest_date',
+
+        // Foreign keys
+        'product_id',
+        'petani_id'     
+    ];
+
+    public function product(): Belongsto
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function petani(): Belongsto
+    {
+        return $this->belongsTo(PetaniData::class, 'petani_id');
+    }
 }
