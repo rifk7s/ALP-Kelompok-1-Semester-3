2552a54fbf86823cf294667978327c2aa89a2398
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-12-12 20:00:27 +0800

    updateRelationshipBetweenUser&Notification

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class, 'buyer_id');
     }
+
+    public function notifications(): HasMany
+    {
+        return $this->hasMany(Notification::class);
+    }
 }
